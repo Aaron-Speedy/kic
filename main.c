b53dbf33f9d3d71df491b2f20040744dc8e1321c
Aaron-Speedy <aaronspeedy@protonmail.com>
2023-06-03 22:40:38 -0500

    Line and buffer structs

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+  char *str;
+  size_t len;
+} Line;
+
+typedef struct {
+  Line *content;
+  size_t num_lines;
+} Buffer;
+
+int main() {
+
+  return 0;
+}
