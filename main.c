433d9aa96124a5f429c2d30f2debe27a1d3000e7
Aaron-Speedy <aaronspeedy@protonmail.com>
2023-06-05 23:16:47 -0500

    Window size

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/ioctl.h>
 
 #define szstr(str) str, sizeof(str)
 
@@ -18,8 +19,13 @@ typedef struct {
 int main() {
   write(1, szstr("\x1b[?1049h"));
   write(1, szstr("\x1b[2J"));
+
+  struct winsize ws;
+  ioctl(1, TIOCGWINSZ, &ws);
+
   write(1, szstr("Hello, world!\n"));
   sleep(1);
+
   write(1, szstr("\x1b[2J"));
   write(1, szstr("\x1b[?1049l"));
   return 0;
