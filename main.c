1e0b76d028b9c3daab402ddc8f629c5020055a90
Aaron-Speedy <aaronspeedy@protonmail.com>
2023-06-05 23:49:00 -0500

    Cleanup at signals

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
 #include <sys/ioctl.h>
+#include <signal.h>
+#include <stdlib.h>
 
 #define szstr(str) str, sizeof(str)
 
@@ -16,9 +17,23 @@ typedef struct {
   size_t num_lines;
 } Buffer;
 
-int main() {
+void cleanup(void) {
   write(1, szstr("\x1b[?1049h"));
   write(1, szstr("\x1b[2J"));
+  write(1, szstr("\x1b[?1049l"));
+}
+void die() { // Signal won't accept functions where void is specified
+  exit(1);
+}
+
+int main() {
+  // Enter into the alternate buffer, and clear scroll-back
+  write(1, szstr("\x1b[?1049h"));
+  write(1, szstr("\x1b[2J"));
+
+  atexit(cleanup);
+  signal(SIGTERM, die);
+  signal(SIGINT, die);
 
   struct winsize ws;
   ioctl(1, TIOCGWINSZ, &ws);
@@ -26,7 +41,5 @@ int main() {
   write(1, szstr("Hello, world!\n"));
   sleep(1);
 
-  write(1, szstr("\x1b[2J"));
-  write(1, szstr("\x1b[?1049l"));
   return 0;
 }
