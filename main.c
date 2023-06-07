604f3abcbf80e3dbaef1a9d9d79b3360f6decd40
Aaron-Speedy <aaronspeedy@protonmail.com>
2023-06-06 20:10:09 -0500

    Function to move cursor

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <sys/ioctl.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define szstr(str) str, sizeof(str)
 
@@ -35,13 +36,21 @@ void die() { // Signal won't accept functions where void is specified
   exit(1);
 }
 
+void move_cursor(Selection *sel, size_t cursor_line_new, size_t cursor_column_new) {
+  sel->cursor_line = cursor_line_new;
+  sel->cursor_column = cursor_column_new;
+
+  printf("\x1b[%zu;%zuH", cursor_line_new, cursor_column_new);
+  fflush(stdout);
+}
+
 int main() {
-  Selection *first_sel = calloc(1, sizeof(Selection));
+  Selection *sels_head = calloc(1, sizeof(Selection));
 
   // Prepare TUI
   write(1, szstr("\x1b[?1049h")); // Switch to alternate buffer
   write(1, szstr("\x1b[2J")); // Clear the screen and disable scrollback.
-  write(1, szstr("\x1b[0;0H")); // Move cursor to 0, 0
+  move_cursor(sels_head, 0, 0);
 
   atexit(cleanup);
   signal(SIGTERM, die);
