24bf2be88bff94fb755fefe148da9888a235a0b9
= <aaron@lspeedy.org>
2023-10-19 01:23:10 -0500

    Move every command to function and make functions easier to understand

--- main.c
+++ main.c
@@ -40,6 +40,8 @@ typedef struct {
   const char *file_path;
 } Buffer;
 
+struct tb_event tb_event;
+
 void get_ordered_cursors(Selection *sel, Cursor **buf) {
   if (sel->anchor.y < sel->cursor.y) { buf[0] = &sel->anchor; buf[1] = &sel->cursor; return; };
   if (sel->anchor.y > sel->cursor.y) { buf[0] = &sel->cursor; buf[1] = &sel->anchor; return; };
@@ -146,6 +148,143 @@ void set_cursor_y(Cursor *cursor, size_t new_cursor_y, size_t new_line_len) {
   cursor->x = cursor->saved_x > new_line_len ? new_line_len : cursor->saved_x;
 }
 
+void shutdown(Buffer *buffer) {
+  tb_shutdown();
+
+  exit(EXIT_SUCCESS);
+}
+
+void enter_insert_mode(Buffer *buffer) {
+  buffer->mode = MODE_INSERT;
+}
+
+void enter_normal_mode(Buffer *buffer) {
+  buffer->mode = MODE_NORMAL;
+}
+
+void enter_insert_in_new_line_below(Buffer *buffer) {
+  Line new_line = {
+    .content = malloc(1),
+    .len = 0,
+    .cap = 1,
+  };
+  for (int i = 0; i < buffer->num_sels; i++) {
+    insert_line(buffer, &new_line, buffer->sels[i].cursor.y + 1);
+    buffer->sels[i].cursor.saved_x  = 0;
+    buffer->sels[i].cursor.x = 0;
+    buffer->sels[i].cursor.y += 1;
+  }
+  buffer->mode = MODE_INSERT;
+}
+
+void remove_selected_text(Buffer *buffer) {
+  for (int i = 0; i < buffer->num_sels; i++) {
+    Cursor *ends[2] = { 0 };
+    get_ordered_cursors(&buffer->sels[i], ends);
+    int x = ends[0]->x;
+    for (int y = ends[0]->y; y < ends[1]->y; y++) {
+      remove_span(buffer, buffer->lines[y].len - x + 1, x, y);
+      x = 0;
+    }
+    remove_span(buffer, ends[1]->x - x + 1, x, ends[1]->y);
+    ends[1]->x = ends[0]->x;
+    ends[1]->saved_x = ends[0]->saved_x;
+    ends[1]->y = ends[0]->y;
+  }
+}
+
+void move_cursors_down(Buffer *buffer) {
+  for (int i = 0; i < buffer->num_sels; i++) {
+    Cursor *cursor = &buffer->sels[i].cursor;
+    if (cursor->y < buffer->num_lines - 1) {
+      set_cursor_y(cursor, cursor->y + 1, buffer->lines[cursor->y + 1].len);
+      buffer->sels[i].anchor.x = cursor->x;
+      buffer->sels[i].anchor.saved_x = cursor->saved_x;
+      buffer->sels[i].anchor.y = cursor->y;
+      if (cursor->y >= buffer->view_top + tb_height()) {
+        buffer->view_top += 1;
+      }
+    }
+  }
+  // TODO: merge_overlapping_selections()
+}
+
+void move_cursors_up(Buffer *buffer) {
+  for (int i = 0; i < buffer->num_sels; i++) {
+    Cursor *cursor = &buffer->sels[i].cursor;
+    if (cursor->y > 0) {
+      set_cursor_y(cursor, cursor->y - 1, buffer->lines[cursor->y - 1].len);
+      buffer->sels[i].anchor.x = cursor->x;
+      buffer->sels[i].anchor.saved_x = cursor->saved_x;
+      buffer->sels[i].anchor.y = cursor->y;
+      if (cursor->y < buffer->view_top) {
+        buffer->view_top -= 1;
+      }
+    }
+  }
+  // TODO: merge_overlapping_selections()
+}
+
+void reduce_selections_to_cursor(Buffer *buffer) {
+  for (int i = 0; i < buffer->num_sels; i++) {
+    buffer->sels[i].anchor.x = buffer->sels[i].cursor.x;
+    buffer->sels[i].anchor.saved_x = buffer->sels[i].cursor.saved_x;
+    buffer->sels[i].anchor.y = buffer->sels[i].cursor.y;
+  }
+}
+
+void extend_selections_left(Buffer *buffer) {
+  for (int i = 0; i < buffer->num_sels; i++) {
+    Cursor *cursor = &buffer->sels[i].cursor;
+    if (cursor->x > 0) {
+      set_cursor_x(cursor, cursor->x - 1);
+    }
+  }
+  // TODO: merge_overlapping_selections()
+}
+
+void extend_selections_right(Buffer *buffer) {
+  for (int i = 0; i < buffer->num_sels; i++) {
+    Cursor *cursor = &buffer->sels[i].cursor;
+    if (cursor->x < buffer->lines[cursor->y].len) {
+      set_cursor_x(cursor, cursor->x + 1);
+      buffer->sels[i].anchor.x = cursor->x;
+      buffer->sels[i].anchor.saved_x = cursor->saved_x;
+      buffer->sels[i].anchor.y = cursor->y;
+    }
+  }
+  // TODO: merge_overlapping_selections()
+}
+
+void move_cursors_left(Buffer *buffer) {
+  extend_selections_left(buffer);
+  reduce_selections_to_cursor(buffer);
+}
+
+void move_cursors_right(Buffer *buffer) {
+  extend_selections_right(buffer);
+  reduce_selections_to_cursor(buffer);
+}
+
+void insert_at_every_cursor(Buffer *buffer) {
+  for (int i = 0; i < buffer->num_sels; i++) {
+    Cursor *cursor = &buffer->sels[i].cursor;
+    insert(&buffer->lines[cursor->y], (char *)&tb_event.ch, 1, cursor->x);
+    set_cursor_x(cursor, cursor->x + 1);
+    set_cursor_x(&buffer->sels[i].anchor, buffer->sels[i].anchor.x + 1);
+  }
+}
+
+void backspace_at_every_cursor(Buffer *buffer) {
+  for (int i = 0; i < buffer->num_sels; i++) {
+    Cursor *cursor = &buffer->sels[i].cursor;
+    if (cursor->x > 0) {
+      remove_span(buffer, 1, cursor->x - 1, cursor->y);
+      cursor->x -= 1;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   if (argc > 2 || argc < 2) {
     printf("Invalid parameters\n");
@@ -210,147 +349,57 @@ int main(int argc, char **argv) {
   }
 
   tb_set_input_mode(TB_INPUT_ESC | TB_INPUT_MOUSE);
-  struct tb_event ev;
 
   draw_terminal(&buffer);
-  while (tb_poll_event(&ev) == TB_OK) {
-    switch (ev.type) {
+  while (tb_poll_event(&tb_event) == TB_OK) {
+    switch (tb_event.type) {
       case TB_EVENT_KEY: {
         switch (buffer.mode) {
           case MODE_NORMAL: {
-            if (ev.key == TB_KEY_CTRL_Q) {
-              goto end;
+            if (tb_event.key == TB_KEY_CTRL_Q) {
+              shutdown(&buffer);
             }
-            if (ev.ch == 'i') {
-              buffer.mode = MODE_INSERT;
+            if (tb_event.ch == 'i') {
+              enter_insert_mode(&buffer);
             }
-            if (ev.ch == 'o') {
-              Line new_line = {
-                .content = malloc(1),
-                .len = 0,
-                .cap = 1,
-              };
-              for (int i = 0; i < buffer.num_sels; i++) {
-                insert_line(&buffer, &new_line, buffer.sels[i].cursor.y + 1);
-                buffer.sels[i].cursor.saved_x  = 0;
-                buffer.sels[i].cursor.x = 0;
-                buffer.sels[i].cursor.y += 1;
-              }
-
-              buffer.mode = MODE_INSERT;
+            if (tb_event.ch == 'o') {
+              enter_insert_in_new_line_below(&buffer);
             }
-            if (ev.ch == 'd') {
-              for (int i = 0; i < buffer.num_sels; i++) {
-                Cursor *ends[2] = { 0 };
-                get_ordered_cursors(&buffer.sels[i], ends);
-                int x = ends[0]->x;
-                for (int y = ends[0]->y; y < ends[1]->y; y++) {
-                  remove_span(&buffer, buffer.lines[y].len - x + 1, x, y);
-                  x = 0;
-                }
-                remove_span(&buffer, ends[1]->x - x + 1, x, ends[1]->y);
-                ends[1]->x = ends[0]->x;
-                ends[1]->saved_x = ends[0]->saved_x;
-                ends[1]->y = ends[0]->y;
-              }
+            if (tb_event.ch == 'd') {
+              remove_selected_text(&buffer);
             }
-            if (ev.ch == 'j') {
-              for (int i = 0; i < buffer.num_sels; i++) {
-                Cursor *cursor = &buffer.sels[i].cursor;
-                if (cursor->y < buffer.num_lines - 1) {
-                  set_cursor_y(cursor, cursor->y + 1, buffer.lines[cursor->y + 1].len);
-                  buffer.sels[i].anchor.x = cursor->x;
-                  buffer.sels[i].anchor.saved_x = cursor->saved_x;
-                  buffer.sels[i].anchor.y = cursor->y;
-                  if (cursor->y >= buffer.view_top + tb_height()) {
-                    buffer.view_top += 1;
-                  }
-                }
-              }
-              // TODO: merge_overlapping_selections()
+            if (tb_event.ch == 'j') {
+              move_cursors_down(&buffer);
             }
-            if (ev.ch == 'k') {
-              for (int i = 0; i < buffer.num_sels; i++) {
-                Cursor *cursor = &buffer.sels[i].cursor;
-                if (cursor->y > 0) {
-                  set_cursor_y(cursor, cursor->y - 1, buffer.lines[cursor->y - 1].len);
-                  buffer.sels[i].anchor.x = cursor->x;
-                  buffer.sels[i].anchor.saved_x = cursor->saved_x;
-                  buffer.sels[i].anchor.y = cursor->y;
-                  if (cursor->y < buffer.view_top) {
-                    buffer.view_top -= 1;
-                  }
-                }
-              }
-              // TODO: merge_overlapping_selections()
+            if (tb_event.ch == 'k') {
+              move_cursors_up(&buffer);
             }
-            if (ev.ch == 'h') {
-              for (int i = 0; i < buffer.num_sels; i++) {
-                Cursor *cursor = &buffer.sels[i].cursor;
-                if (cursor->x > 0) {
-                  set_cursor_x(cursor, cursor->x - 1);
-                  buffer.sels[i].anchor.x = cursor->x;
-                  buffer.sels[i].anchor.saved_x = cursor->saved_x;
-                  buffer.sels[i].anchor.y = cursor->y;
-                }
-              }
-              // TODO: merge_overlapping_selections()
+            if (tb_event.ch == 'h') {
+              move_cursors_left(&buffer);
             }
-            if (ev.ch == 'H') {
-              for (int i = 0; i < buffer.num_sels; i++) {
-                Cursor *cursor = &buffer.sels[i].cursor;
-                if (cursor->x > 0) {
-                  set_cursor_x(cursor, cursor->x - 1);
-                }
-              }
-              // TODO: merge_overlapping_selections()
+            if (tb_event.ch == 'H') {
+              extend_selections_left(&buffer);
             }
-            if (ev.ch == 'l') {
-              for (int i = 0; i < buffer.num_sels; i++) {
-                Cursor *cursor = &buffer.sels[i].cursor;
-                if (cursor->x < buffer.lines[cursor->y].len) {
-                  set_cursor_x(cursor, cursor->x + 1);
-                  buffer.sels[i].anchor.x = cursor->x;
-                  buffer.sels[i].anchor.saved_x = cursor->saved_x;
-                  buffer.sels[i].anchor.y = cursor->y;
-                }
-              }
-              // TODO: merge_overlapping_selections()
+            if (tb_event.ch == 'l') {
+              move_cursors_right(&buffer);
             }
-            if (ev.ch == 'L') {
-              for (int i = 0; i < buffer.num_sels; i++) {
-                Cursor *cursor = &buffer.sels[i].cursor;
-                if (cursor->x < buffer.lines[cursor->y].len) {
-                  set_cursor_x(cursor, cursor->x + 1);
-                }
-              }
-              // TODO: merge_overlapping_selections()
+            if (tb_event.ch == 'L') {
+              extend_selections_right(&buffer);
             }
-            if (ev.key == TB_KEY_ESC) {
+            if (tb_event.key == TB_KEY_ESC) {
               write_buffer_to_file(&buffer);
             }
           } break;
 
           case MODE_INSERT: {
-            if (ev.ch >= ' ' && ev.ch <= '~') {
-              for (int i = 0; i < buffer.num_sels; i++) {
-                Cursor *cursor = &buffer.sels[i].cursor;
-                insert(&buffer.lines[cursor->y], (char *)&ev.ch, 1, cursor->x);
-                set_cursor_x(cursor, cursor->x + 1);
-                set_cursor_x(&buffer.sels[i].anchor, buffer.sels[i].anchor.x + 1);
-              }
+            if (tb_event.ch >= ' ' && tb_event.ch <= '~') {
+              insert_at_every_cursor(&buffer);
             }
-            if (ev.key == TB_KEY_ESC) {
-              buffer.mode = MODE_NORMAL;
+            if (tb_event.key == TB_KEY_ESC) {
+              enter_normal_mode(&buffer);
             }
-            if (ev.key == TB_KEY_CTRL_8) { // Backspace 
-              for (int i = 0; i < buffer.num_sels; i++) {
-                Cursor *cursor = &buffer.sels[i].cursor;
-                if (cursor->x > 0) {
-                  remove_span(&buffer, 1, cursor->x - 1, cursor->y);
-                  cursor->x -= 1;
-                }
-              }
+            if (tb_event.key == TB_KEY_CTRL_8) { // Backspace 
+              backspace_at_every_cursor(&buffer);
             }
           } break;
         }
@@ -359,9 +408,7 @@ int main(int argc, char **argv) {
     draw_terminal(&buffer);
   }
 
-end:
-  tb_shutdown();
-
+  shutdown(&buffer);
   return 0;
 }
 
